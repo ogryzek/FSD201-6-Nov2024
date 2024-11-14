3ab3b70633f6b00bfbc7cdc431d1abf235496878
Drew Ogryzek <drew@molg.ai>
2024-11-13 18:44:38 -0800

    Adds some more stuff

--- Week2/server.js
+++ Week2/server.js
@@ -6,20 +6,23 @@ and responds with the relevant data.json
 */
 
 const http = require('node:http');
+const fs = require('node:fs');
 
 http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
     const baseURL = req.protocol + '://' + req.headers.host + '/';
     const reqUrl = new URL(req.url, baseURL);
     const path = reqUrl.pathname;
+    let data = '';
     
     if (path === "/api") { 
-        res.write("This should show the data.json");
-
+        data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+        console.log(JSON.parse(data));
+        res.setHeader('Content-Type', 'application/json');
     }
     else {
-        res.write("<h1>Welcome to this page</h1>");
+        res.setHeader('Content-Type', 'text/html');
+        data = "<h1>Welcome to this page</h1>";
     }
 
-    res.end();
+    res.end(data);
 }).listen(3000);

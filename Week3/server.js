540eec22a247d62fedd20176ce35ed701e20d89c
Drew Ogryzek <drew@molg.ai>
2024-11-19 20:07:00 -0800

    Serve an HTML web page

--- Week3/server.js
+++ Week3/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.get('/', (req, resp) => {
     resp.send('Hello there!');
+});
+
+app.get('/firstpage', (req, res) => {
+    fs.readFile('index.html', (err, data) => {
+        res.send(data.toString());
+    });
 });
 
 app.listen(port);

be7cda413789338eed27295e33ab726e398c87cf
Drew Ogryzek <drew@molg.ai>
2024-11-20 19:02:59 -0800

    Answers week3 exercise 03

--- Week3/crud_api/server.js
+++ Week3/crud_api/server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 const toys = JSON.parse(fs.readFileSync("./data/data.json", 'utf-8'));
+app.use(express.json())
 
 function getNewToyID() {
     const lastToy = toys[toys.length -1];
@@ -17,7 +18,9 @@ app.get('/api/v1/toys', (req, res) => {
 });
 
 app.post('/api/v1/toys', (req, res) => {
+    req.body;
     const newToy = Object.assign({id: getNewToyID()}, req.body)
+    console.log(`the new toy is: ${JSON.stringify(newToy)}`);
     toys.push(newToy);
 
     fs.writeFile("./data/data.json", JSON.stringify(toys), err => {

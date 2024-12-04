82800a0cb4b850f1527a56d097710700895eb67e
Drew Ogryzek <drew@molg.ai>
2024-12-03 20:31:38 -0800

    Adds some Week5 MongoDB stuff

---
+++ Week5/MongoDBApp/b2.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = <connection-string>
+// Create a new client and connect to MongoDB
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    // Connect to the "insertDB" database and access its "haiku" collection
+    const foods = client.db("foods");
+    const fruit = foods.collection("fruit");
+    
+    // Create a document to insert
+    const lemon = {
+        name: "lemon",
+        price: 0.5,
+        description: "Yellow and sour!"
+    }
+    // Insert the defined document into the "haiku" collection
+    const result = await fruit.insertOne(lemon);
+    // Print the ID of the inserted document
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+     // Close the MongoDB client connection
+    await client.close();
+  }
+}
+// Run the function and handle any errors
+run().catch(console.dir);
---
+++ Week5/README.md
@@ -0,0 +1,19 @@
+# Week 5  
+  
+Create a Mongo DB account and database.  
+  
+https://docs.mongodb.com/manual/tutorial/getting-started  
+
+New User
+
+```
+username: MyUserName
+password: MySecretPassword
+
+```
+
+Select driver to connect to database: Node.js and make sure to install the appropriate driver:  
+  
+```
+npm install mongodb
+```
